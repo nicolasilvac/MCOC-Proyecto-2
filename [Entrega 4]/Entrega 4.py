f14343dda7312ec44a5078e13aa0b9713935918e
TomasArteaga <53574632+TomasArteaga@users.noreply.github.com>
2019-10-03 14:56:59 -0300

    Add files via upload

--- [Entrega 4]/Entrega 4.py
+++ [Entrega 4]/Entrega 4.py
@@ -1,88 +1,86 @@
-#Entrega 3 MCOC-Proyecto-2
 from matplotlib.pylab import *
 
-#Unidades base
-
-_m = 1. #metros
+#Unidades base son SI (m, kg, s)
+_m = 1.
 _kg = 1.
 _s = 1.
-_mm = 10.**-3*_m 
-_gr = 10.**-3*_kg
+_mm = 1e-3*_m
+_cm = 1e-3*_m
+_gr = 1e-3*_kg
 
-vfx= 5.*_m/_s # m/s
-vfy= 0.*_m/_s # m/s
 
-x0 = array([0.,1.], dtype = double) #posicion actual
-v0 = array([1.,1.], dtype = double) #velocidad actual
+vfx = -10.*_m/_s
+vfy = .1*_m/_s
 
-xi = x0 #posicion siguiente #vector tamano 2
-vi = v0 #velocidad siguiente #vector tamano 2
-xim1 = zeros(2.,dtype=double)
-vim1 = zeros(2.,dtype=double)
+x0 = array([0.,1.*_mm], dtype = double)
+v0 = array([1.,1.], dtype = double)
+
+xi= x0 #zeros(2, dtype = double) posicion
+vi= v0 #zeros(2, dtype = double) velocidad
+xim1 = zeros(2, dtype = double) #posicion siguiente
+vim1 = zeros(2, dtype = double) #velocidad siguiente
 
 g = 9.81*_m/_s**2
 d = 1.*_mm
-rho = 2700.*_kg/(_m**3)
-Cd = 0.47 #Para particula esferica
-m = rho*pi*(d**3)*(4./3./8.) #Masa de particula (grano de arena)
-	#Inicializar Euler en x0
-	
-dt = (2e-6)*_s 	#Paso de tiempo
-tmax = 1.0*_s #Tiempo max de simulacion
-ti = 0.0*_s #Tiempo inicial
+rho_agua = 1000.*_kg/(_m**3)
+rho_particula = 2650.*_kg/(_m**3)
+Cd = 0.47 #para una particula
 
-w = array([0,-m*g]) #vector peso
-vf = array([vfx,vfy]) #matriz de  vfx filas y vfy columnas
+A = pi*(d/2)**3
+V = (4./3.)*pi*(d/2)**3
+m = rho_particula*V
 
-Nt = int32(2*tmax/dt) #numeros de paso del tiempo, tiene que ser entero
-x_store = zeros((2,Nt)) #guardar posiciones en cada tiempo 
-v_store = zeros((2,Nt)) #guardar velocidades en cada timepo
-t_store = zeros((Nt)) #solo un tiempo, arreglos con 0
-#Metodo de euler
-i=0
-while ti<tmax:
+dt = 0.001*_s	#paso de tiempo
+tmax = 2*_s		#tiempo maximo de simulacion
+ti = 0.*_s		#tiempo actual
 
-	if i % 100 == 0:
-		print 'ti=',ti ," |xi| = ", sqrt(dot(xi,xi))
-	#print 'xi=',xi
-	#print 'vi=',vi
+W = array([0, -m*g])
+fB = array([0, rho_agua*V*g])
 
-	#Evaluar v. relativa
-	vrel= vf-vi #vf=velocidad del flujo
-	norm_vrel= sqrt(dot(vrel,vrel)) #norma de vrel
+t = arange(0, tmax, dt)
+Nt = len(t)
 
-	#Evaluar fuerzas sobre particulas
+norm = lambda v: sqrt(dot(v,v))
 
-	fD = 0.5 * Cd * norm_vrel * vrel #Fuerza drag
-	Fi = w + fD
-	#Hasta ahora lo que hay es la fuerza que hay en cada particula en cada tiempo discreto
+k_penal = 1000.*0.5*Cd*rho_agua*A*norm(v0)/(1*_mm)
 
-	#Evaluar aceleracion
-	ai = Fi / m
+def particula(z, t):
+	xi = z[:2]
+	vi = z[2:]
+	vf = array([vfx, vfy])
+	vrel = vf - vi
+	fD = (0.5*Cd*rho_agua*A*norm(vrel))*vrel
+	# fL = 3.0/4.0*alpha*Cd*()
+	Fi = W + fD + fB
 
-	#Integrar
-	xim1 = xi + vi*dt + ai*(dt**2/2)
-	vim1 = vi + ai*dt
-	
-	#Avanzar al siguiente paso
-	x_store[:,i] = xi #se guarda el componente actual
-	v_store[:,i] = vi
-	t_store[:] = ti
-
-	ti += dt
-	i += 1
-	xi = xim1
-	vi = vim1
-
-#guardar ultimo paso
-x_store[:,i] = xi
-v_store[:,i] = vi
-t_store[i] = ti
+	if xi[1] < 0:
+		Fi[1] += -k_penal*xi[1]
 
 
-print x_store
+
+	zp = zeros(4)
+	zp[:2] = vi
+
+
+	zp[2:] = Fi/m
+	return zp
+
+
+from scipy.integrate import odeint
+z0 = zeros(4)
+z0[:2] = x0
+z0[2:] = v0
+z = odeint(particula, z0, t)
+x = z[:,:2]
+v = z[:,2:]
 
 figure()
-plot(x_store[0,:i],x_store[1,:i])
-show()
+subplot(2,1,1)
+plot(t,x[:,0], label = 'x')
+plot(t,x[:,1], label = 'y')
+plt.legend()
+subplot(2,1,2)
+plot(t,v[:,0], label = 'vx')
+plot(t,v[:,0], label = 'vy')
 
+show()
