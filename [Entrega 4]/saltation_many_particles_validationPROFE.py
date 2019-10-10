447240ca2166fa4aa2e65a591a1b8b588b788a26
nicolasilvac <53713677+nicolasilvac@users.noreply.github.com>
2019-10-10 17:54:15 -0300

    Add files via upload

--- [Entrega 4]/saltation_many_particles_validationPROFE.py
+++ [Entrega 4]/saltation_many_particles_validationPROFE.py
@@ -1,6 +1,7 @@
 from scipy.integrate import odeint
 from matplotlib.pylab import * 
 import random
+
 #Unidades base son SI (m, kg, s)
 _m = 1.
 _kg = 1.
@@ -11,17 +12,17 @@ _gr = 1e-3*_kg
 _in = 2.54*_cm
 
 g = 9.81*_m/_s**2
-d = 0.56*_mm
+d = 0.56*_cm
 
 rho_agua = 1000. *_kg/(_m**3)
 rho_particula = 2650. *_kg/(_m**3)
-
 
 dt = 0.001*_s	#paso de tiempo
 tmax =0.5#0.1 * _s   #tiempo maximo de simulacion
 ti = 0. * _s #tiempo actual
 
-Nparticulas = 10
+Nparticulas = 3
+
 
 x0 = 100*d*rand(Nparticulas)
 y0 = 30*d*rand(Nparticulas) + d
@@ -53,7 +54,6 @@ ihat = array([1,0])
 jhat = array([0,1])
 
 ustar = 0.14
-
 def velocity_field(x):
 	z = x[1]/d
 	if z > 1./30:
@@ -62,10 +62,9 @@ def velocity_field(x):
 		uf = 0
 	return array([uf,0]) 
 
+
 vfx= velocity_field([0, 4*d])[0]
-k_penal = 1000*0.5*Cd*rho_agua*A*norm(vfx)**2/(1*_mm)
-
-
+k_penal = 100*0.5*Cd*rho_agua*A*norm(vfx)**2/(1*_mm)
 
 
 def particula(z,t):
@@ -80,13 +79,10 @@ def particula(z,t):
 		vf_top = norm(velocity_field(xi + (di/2)*jhat))
 		vf_bot = norm(velocity_field(xi - (di/2)*jhat))
 		vrel = vf - vi
-
 		fD = (0.5*Cd*alpha*rho_agua*norm(vrel)*A)*vrel
 		fL = (0.5*CL*alpha*rho_agua*(vf_top**2 - vf_bot**2)*A)*jhat
 
 		Fi = W + fD + fL
-
-		
 
 		if xi[1] < 0:
 			Fi[1] += -k_penal*xi[1]
@@ -124,19 +120,20 @@ print "Fin"
 fig = figure()
 
 ax= gca()
+
+
 for i in range(Nparticulas):
 	if i % 2 == 0:
 		xi = z[:, 4*i]
 		yi = z[:, 4*i+1]
 		col = rand(4)
 		plot(xi,yi,"--,",color=col)
-		#ylim(0,10*_mm)
-
+		ylim(-.2,10*_mm)
 		for x,y in zip(xi, yi):
 			ax.add_artist(Circle(xy=(x,y),radius=d/2,color=col,alpha= 0.7))
-#ax.axhline(d/2,color="k",linestyle="--")
-#
-#axis("equal")
+ax.axhline(0,color="k",linestyle="--")
+
+axis("equal")
 
 
 show()
