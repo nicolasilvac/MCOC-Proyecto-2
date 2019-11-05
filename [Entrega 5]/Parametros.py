24283bf4d826c2815c8e260b968466bc69825a85
Vicente Carreño Villagrán <53504276+VicenteCV@users.noreply.github.com>
2019-11-05 00:38:36 -0300

    Update Parametros.py
    Adición de parámetros y definición de estos, corrección de parámetros.

--- [Entrega 5]/Parametros.py
+++ [Entrega 5]/Parametros.py
@@ -10,32 +10,42 @@ _cm = 1e-2*_m
 _gr = 1e-3*_kg
 _in = 2.54*_cm
 
-g = 9.81*_m/_s**2
-d = 0.15*_mm
 
+
+#parametros fisicos
+g = 9.81*_m/_s**2
 rho_agua = 1000.*_kg/(_m**3)		# Densidad del agua
 rho_particula = 2650.*_kg/(_m**3)	# Densidad de las particulillas
 
+#transcurso del tiempo
 dt = 0.0001*_s    	# paso de tiempo 
 tmax = 2*_s      # tiempo maximo de simulacion
 
+#parametros geometricos y peso
+d = 0.15*_mm
+A = pi*(d/2)**2 		#Area Particula
+V = (4./3.)*pi*(d/2)**3 #Volumen Particula
+m = rho_particula*V 	#Masa particula
+
+#coeficientes para formula
 Cd = 0.47    #Coeficiente de arrastre
 Cm = 0.5	#Masa adicional
 CL = 0.2	#Coeficiente de lift
-Rp = 73.  # Errepe
-
+Rp = 73.  
+R = (rho_particula/rho_agua - 1)  
 ustar = 0.14	#  (m/s)  0.14 - 0.23 usestrella
-
-Nparticulas = 20	#Numero de particulas
-
+alpha = 1/(1 + R + Cm)			
 tau_star = 0.067   # Tau star (Shear stress)
 
 
-R = (rho_particula/rho_agua - 1)   # Esta es la letra erre
-alpha = 1/(1 + R + Cm)			# A en griego, de aqui viene la palabra "alfabeto"
 
+
+Nparticulas = 20	#Numero de particulas
+
+#Directores
 ihat = array([1,0])			# itongo
 jhat = array([0,1])			# jotatongo
+norm=lambda v: sqrt(dot(v,v))
 
 tau_cr = 0.22*Rp**(-0.6)+0.06*10**(-7*Rp**(-0.6))   # tau critico
 ustar = sqrt(tau_star * g * Rp * d)		# uestrella de verdad ahora si final final ok para siempre
@@ -69,3 +79,4 @@ def velocity_field(x):
 vfx = velocity_field([0, 10*d])[0]
 A = pi*(d/2)**2
 k_penal = 0.5*Cd*rho_agua*A*norm(vfx)**2/(d/20)
+
